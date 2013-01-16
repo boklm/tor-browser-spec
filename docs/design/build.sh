c9da119126462ad40d075712f47cdd60813c8568
Mike Perry <mikeperry-git@fscked.org>
2013-01-15 23:42:17 -0800

    Begin work on design doc updates for Firefox 17.

--- docs/design/build.sh
+++ docs/design/build.sh
@@ -1,1 +1,1 @@
-xsltproc  --output index.html.en  --stringparam section.autolabel.max.depth 2 --stringparam  section.autolabel 1 /usr/share/sgml/docbook/xsl-stylesheets-1.75.2/xhtml/docbook.xsl design.xml 
+xsltproc  --output index.html.en  --stringparam section.autolabel.max.depth 2 --stringparam  section.autolabel 1 /usr/share/xml/docbook/stylesheet/docbook-xsl/xhtml-1_1/docbook.xsl design.xml 
